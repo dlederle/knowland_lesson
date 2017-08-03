317c3f82c9183635f4e1d83cf5b5c0d48985b254
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2017-08-03 07:42:06 -0400

    simple animation

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
+var radius = 40;
+var x = -radius;
+var speed = 0.5;
 
 //Setup is called by p5 once
 function setup() {
   createCanvas(800, 400);
+  ellipseMode(RADIUS);
 }
 
 //Draw is called by p5 each frame
 function draw() {
-  background(255, 255, 255); //R, G, B
-  ellipse(50, 50, 80, 80);
+  background(0);
+  x += speed;
+  arc(x, 60, radius, radius, 0.52, 5.76);
 }
