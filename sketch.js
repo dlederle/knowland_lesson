e2f0e323048dc059531b9c3afbaac7686f33b85a
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2017-08-03 07:12:31 -0400

    mouse follow

--- sketch.js
+++ sketch.js
@@ -6,6 +6,11 @@ function setup() {
 
 //Draw is called by p5 each frame
 function draw() {
-  background(255, 255, 255); //R, G, B
-  ellipse(50, 50, 80, 80);
+  //background(255, 255, 255); //R, G, B
+  if(mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
